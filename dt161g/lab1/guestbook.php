6c1915527fe5c16bf474389670bf8fa40d43cfae
Henrik Henriksson <hehe0601@student.miun.se>
2020-01-29 14:10:27 +0100

    Fixed IP-Address getter, updated comments

--- dt161g/lab1/guestbook.php
+++ dt161g/lab1/guestbook.php
@@ -15,7 +15,7 @@ $title = "Laboration 1";
 session_start();
 date_default_timezone_set('Europe/Stockholm');
 //---------------------------------------------------------------------------
-// $filename = "posts.json";
+// Set filename path to the writable folder
 $filename = __DIR__ . "/../../writeable/posts.json";
 
 // load posts from json file.
@@ -32,14 +32,14 @@ $iName = '';
 $iText = '';
 $ErrorMessage = '';
 
-// check wether something has been previously submitted
+// check whether something has been previously submitted
 if (!empty($_POST)) {
     // check if the entered captcha was valid:
     if ($_POST['captcha'] === $_SESSION["sCap"]) {
         // store the values the user has entered.
         storePosts($posts);
         printToFile($posts, $filename);
-        // set cookie
+        // set cookie, currently set to 5 minutes for testing.
         setcookie('HasPosted', gethostname(), time() + (60 * 5));
         // Refresh the Page:
         header("Refresh:0");
@@ -56,6 +56,8 @@ $_SESSION["sCap"] = $randCaptcha;
 //---------------------------------------------------------------------------
 // Function Defenitions:
 //---------------------------------------------------------------------------
+
+// this function generates a unique captcha of 5 chars from a selection specified in the seed variable.
 function generateCaptcha()
 {
     // Generate random captcha code:
@@ -70,50 +72,54 @@ function generateCaptcha()
     return $randomized;
 }
 //---------------------------------------------------------------------------
-// function printStoredPosts(array $posts)
-// {
-//     foreach ($posts as $key) {
-
-//         echo  "<tr>
-//            <td>{$key['name']}</td>
-//            <td>{$key['text']}</td>
-//            <td> IP: {$key['ip']}<br> Tid: {$key['time']}</td>
-//            </tr>";
-//     }
-// }
-//---------------------------------------------------------------------------
+// store the post to the guestbook array
 function storePosts(array &$posts)
 {
     $posts[] = [
         'name' => trim(htmlspecialchars($_POST["name"])),
         'text' => trim(htmlspecialchars($_POST["text"])),
-        'ip' => gethostbyname(gethostname()),
+        'ip' => getIP(),
         'time' => date("Y-m-d H:i:s", time())
     ];
 }
 //---------------------------------------------------------------------------
+// checks for the users ip adress, also attempts to get the correct ip address even if the user has a proxy.
+function getIP()
+{
+    if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+        $ip_address = $_SERVER['HTTP_CLIENT_IP'];
+    }
+    //whether ip is from proxy
+    elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        $ip_address = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    }
+    //whether ip is from remote address
+    else {
+        $ip_address = $_SERVER['REMOTE_ADDR'];
+    }
+    return $ip_address;
+}
+
+//---------------------------------------------------------------------------
+// Read from file. First checks to make sure the file is readable.
 function readFromFile($filename)
 {
 
     if (is_readable($filename)) {
         $postsJson = file_get_contents($filename);
-        $parsedPosts = json_decode($postsJson, true);
-        return $parsedPosts;
+        return json_decode($postsJson, true);
     } else {
         return [];
     }
 }
 //---------------------------------------------------------------------------
+// print the guestbook posts array to file.
 function printToFile(array &$posts, $filename)
 {
 
     $postsJson = json_encode($posts);
     file_put_contents($filename, $postsJson);
-    // file_put_contents($filename, $postsJson);
-    // file_put_contents(__DIR__ . '/posts.json', $postsJson);
-
 }
-
 
 /*******************************************************************************
  * HTML section starts here
@@ -130,78 +136,78 @@ function printToFile(array &$posts, $filename)
 </head>
 
 <body>
-<header>
-    <img src="img/mittuniversitetet.jpg" alt="miun logga" class="logo" />
-    <h1><?php echo $title ?></h1>
-</header>
-<main>
-    <aside>
-        <h2>LOGIN</h2>
-        <form action="index.php">
-            <label><b>Username</b></label>
-            <input type="text" placeholder="Enter Username" name="uname" required maxlength="10">
-            <label><b>Password</b></label>
-            <input type="password" placeholder="Enter Password" name="psw" required>
-            <button type="submit">Login</button>
-        </form>
-        <h2>MENY</h2>
-        <nav>
-            <ul>
-                <li>
-                    <a href="index.php">HEM</a>
-                </li>
-
-            </ul>
-        </nav>
-    </aside>
-    <section>
-        <h2>GÄSTBOK</h2>
-        <table>
-            <tr>
-                <th class="th20">FRÅN
-                </th>
-                <th class="th40">INLÄGG
-                </th>
-                <th class="th40">LOGGNING
-                </th>
-            </tr>
-            <!-- create a  new table row for each post in the posts array -->
-            <?php foreach ($posts as $key) : ?>
-                <tr>
-                    <td><?php echo $key['name'] ?></td>
-                    <td><?php echo $key['text'] ?> </td>
-                    <td> <?php echo "IP:{$key['ip']}" ?> <br>
-                        <?php echo "Tid: {$key['time']}" ?> </td>
-                </tr>
-            <?php endforeach; ?>
-        </table>
-
-
-        <!-- Only print the form if the user has not previously posted: -->
-        <?php if (!(isset($_COOKIE["HasPosted"]))) : ?>
-            <form action="guestbook.php" method="POST">
-                <fieldset>
-                    <legend>Skriv i gästboken</legend>
-                    <label>Från: </label>
-                    <input type="text" placeholder="Skriv ditt namn" name="name" value="<?php echo $iName; ?>" required>
-                    <br>
-                    <label for="text">Inlägg</label>
-                    <textarea id="text" name="text" rows="10" cols="50" placeholder="Skriva meddelande här" required><?php echo $iText; ?></textarea>
-                    <br>
-                    <label>Captcha: <span class="red"><?php echo $randCaptcha; ?></span></label>
-                    <input type="text" placeholder="Skriva captcha här" name="captcha" required>
-                    <button type="submit">Skicka</button>
-                    <br>
-                    <h3 class="red"><?php echo $ErrorMessage; ?></h3>
-                </fieldset>
+    <header>
+        <img src="img/mittuniversitetet.jpg" alt="miun logga" class="logo" />
+        <h1><?php echo $title ?></h1>
+    </header>
+    <main>
+        <aside>
+            <h2>LOGIN</h2>
+            <form action="index.php">
+                <label><b>Username</b></label>
+                <input type="text" placeholder="Enter Username" name="uname" required maxlength="10">
+                <label><b>Password</b></label>
+                <input type="password" placeholder="Enter Password" name="psw" required>
+                <button type="submit">Login</button>
             </form>
-        <?php endif; ?>
+            <h2>MENY</h2>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="index.php">HEM</a>
+                    </li>
 
-    </section>
-</main>
-<footer>
-    Footer
-</footer>
+                </ul>
+            </nav>
+        </aside>
+        <section>
+            <h2>GÄSTBOK</h2>
+            <table>
+                <tr>
+                    <th class="th20">FRÅN
+                    </th>
+                    <th class="th40">INLÄGG
+                    </th>
+                    <th class="th40">LOGGNING
+                    </th>
+                </tr>
+                <!-- create a  new table row for each post in the posts array -->
+                <?php foreach ($posts as $key) : ?>
+                    <tr>
+                        <td><?php echo $key['name'] ?></td>
+                        <td><?php echo $key['text'] ?> </td>
+                        <td> <?php echo "IP:{$key['ip']}" ?> <br>
+                            <?php echo "Tid: {$key['time']}" ?> </td>
+                    </tr>
+                <?php endforeach; ?>
+            </table>
+
+
+            <!-- Only print the form if the user has not previously posted: -->
+            <?php if (!(isset($_COOKIE["HasPosted"]))) : ?>
+                <form action="guestbook.php" method="POST">
+                    <fieldset>
+                        <legend>Skriv i gästboken</legend>
+                        <label>Från: </label>
+                        <input type="text" placeholder="Skriv ditt namn" name="name" value="<?php echo $iName; ?>" required>
+                        <br>
+                        <label for="text">Inlägg</label>
+                        <textarea id="text" name="text" rows="10" cols="50" placeholder="Skriva meddelande här" required><?php echo $iText; ?></textarea>
+                        <br>
+                        <label>Captcha: <span class="red"><?php echo $randCaptcha; ?></span></label>
+                        <input type="text" placeholder="Skriva captcha här" name="captcha" required>
+                        <button type="submit">Skicka</button>
+                        <br>
+                        <h3 class="red"><?php echo $ErrorMessage; ?></h3>
+                    </fieldset>
+                </form>
+            <?php endif; ?>
+
+        </section>
+    </main>
+    <footer>
+        Footer
+    </footer>
 </body>
 
 </html>
