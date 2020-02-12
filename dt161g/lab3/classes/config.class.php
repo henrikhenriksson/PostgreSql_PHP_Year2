c04ab49000c49cbfb87b139d4e275b80b34136ba
Henrik Henriksson <hehe0601@student.miun.se>
2020-02-12 11:32:48 +0100

    Written getters for global variables.

--- dt161g/lab3/classes/config.class.php
+++ dt161g/lab3/classes/config.class.php
@@ -10,13 +10,39 @@
  * hehe0601@student.miun.se
  ******************************************************************************/
 
-
+// require_once "../config.php";
+require_once __DIR__ . "/../config.php";
 class Config
 {
+    private $dsn = "";
+    private $debug = true;
 
     public function getDbDsn()
     {
-        $dsn = "";
-        return $dsn;
+        return $this->dsn;
+    }
+
+    public function getCaptchaLength()
+    {
+        return $GLOBALS['captchaLenght'];
+    }
+    public function getHost()
+    {
+        return $GLOBALS['host'];
+    }
+    public function getUser()
+    {
+        // global $user;
+        // return $user;
+        // require "../config.php";
+        return $GLOBALS['user'];
+    }
+    public function isDebug()
+    {
+        return $this->debug;
+    }
+    public function setDebub(Bool $value)
+    {
+        $this->debug = $value;
     }
 }
