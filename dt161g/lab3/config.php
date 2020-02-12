dbdfb62ba5edb8f530aea3fa7446dfe03b05840b
Henrik Henriksson <hehe0601@student.miun.se>
2020-02-12 11:33:07 +0100

    Added variables to GLOBALS superglobal

--- dt161g/lab3/config.php
+++ dt161g/lab3/config.php
@@ -9,3 +9,13 @@
  * hehe0601
  * hehe0601@student.miun.se
  ******************************************************************************/
+
+// initiate variables:
+
+$GLOBALS["host"] = "localhost"; // database host path
+$GLOBALS["user"] = "hehe0601";  // User name
+$GLOBALS["captchaLenght"] = 5; // length of the captcha to be generated.
+
+// define("HOST", "localhost", true);
+// define("USER", "hehe0601" true);
+// define("CAPTCHALENGTH", 5, true); // constant holding the length of the captcha to be generated.
