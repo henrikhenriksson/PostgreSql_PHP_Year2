db30f95b6a7bbd4b4ca70f1e39c7aa86ec3a2c58
Henrik Henriksson <hehe0601@student.miun.se>
2020-02-14 10:12:40 +0100

    Rewritten add query logic to add insertion protection

--- dt161g/lab3/classes/dbHandler.class.php
+++ dt161g/lab3/classes/dbHandler.class.php
@@ -3,14 +3,14 @@
 /*******************************************************************************
  * Laboration 3, Kurs: DT161G
  * File: config.php
- * Desc: Config file for laboration 3
+ * Desc: dbHandler class file for laboration 3
  *
  * Henrik Henriksson
  * hehe0601
  * hehe0601@student.miun.se
  ******************************************************************************/
 
-
+// this class is responsible for handling requests to the database.
 class dbHandler
 {
     private $dbconn;
@@ -20,26 +20,40 @@ class dbHandler
         $dbconn = null;
     }
 
+    // establish a connection to the server.
     public function connect()
     {
         require __DIR__ . "/../util.php";
         $this->dbconn = pg_connect($config->getDbDsn());
         return $this->dbconn;
     }
+    // disconnect from the server.
     public function disconnect()
     {
         pg_close($this->dbconn);
     }
 
+    // Store a post to the database.
     function storePostToDatabase(array $post)
     {
         if ($this->connect()) {
 
-            $queryStr = "INSERT INTO dt161g.guestbook (name, message, iplog, timelog) VALUES('$post[name]', '$post[message]', '$post[iplog]', '$post[timelog]')";
-            $result = pg_query($this->dbconn, $queryStr);
+            $queryStr = "INSERT INTO dt161g.guestbook (name, message, iplog, timelog) VALUES($1,$2,$3,$4)";
+
+            // by refering to the values as params we get some protection from code injections to the database.
+            $result = pg_query_params($this->dbconn, $queryStr, $post);
+
+            // returns message if the request was unsuccessful.
+            if (!$result) {
+                echo "Error sending request: <br>\n";
+                pg_last_error($this->dbconn);
+            }
             $this->disconnect();
         }
     }
+
+    // Load the posts from the database to print on the website.
+    // returns a Post[]
     function getPostsFromDatabase()
     {
 
@@ -50,13 +64,14 @@ class dbHandler
             $queryStr = "SELECT * FROM dt161g.guestbook;";
             $result = pg_query($this->dbconn, $queryStr);
 
+            // the query fetches an object, which is then inserted into a Post class object that is then added to an array of Post objects.
             for ($i = 0; $i < pg_num_rows($result); $i++) {
                 $databaseObj = pg_fetch_object($result);
                 $fetchedPost = new Post($databaseObj->name, $databaseObj->message, $databaseObj->iplog, $databaseObj->timelog);
 
                 $databasePosts[] = $fetchedPost;
             }
-
+            // free the resources.
             pg_free_result($result);
             $this->disconnect();
         }
