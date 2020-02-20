2567574ed345fb749819d02d56172d40a7493a6a
Henrik Henriksson <hehe0601@student.miun.se>
2020-02-20 09:56:52 +0100

    Documentation

--- dt161g/lab4/classes/member.class.php
+++ dt161g/lab4/classes/member.class.php
@@ -1,15 +1,16 @@
 <?php
+
 /*******************************************************************************
  * Laboration 4, Kurs: DT161G
  * File: member.class.php
  * Desc: Class Member for laboration 4
  *
- * Anders Student
- * ansu6543
- * ansu6543@student.miun.se
+ * Henrik Henriksson
+ * hehe0601
+ * hehe0601@student.miun.se
  ******************************************************************************/
 
 
-class Member {
-
+class Member
+{
 }
--- dt161g/lab4/classes/role.class.php
+++ dt161g/lab4/classes/role.class.php
@@ -1,15 +1,16 @@
 <?php
+
 /*******************************************************************************
  * Laboration 4, Kurs: DT161G
  * File: role.class.php
  * Desc: Class Role for laboration 4
  *
- * Anders Student
- * ansu6543
- * ansu6543@student.miun.se
+ * Henrik Henriksson
+ * hehe0601
+ * hehe0601@student.miun.se
  ******************************************************************************/
 
 
-class Role {
-
+class Role
+{
 }
