9a236e2d894a8053293834f1bae90ff8dd01b805
Henrik Henriksson <hehe0601@student.miun.se>
2020-02-21 11:28:20 +0100

    Started writing member class

--- dt161g/lab4/classes/member.class.php
+++ dt161g/lab4/classes/member.class.php
@@ -13,4 +13,46 @@
 
 class Member
 {
+    private $id;
+    private $userName;
+    private $password;
+
+
+    public function __construct(int $pId, string $pUserName, string $pPassword)
+    {
+        $this->id = $pId;
+        $this->userName = $pUserName;
+        $this->password = $pPassword;
+    }
+
+    /**
+     * Get the value of id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+    /**
+     * Get the value of userName
+     */
+    public function getUserName()
+    {
+        return $this->userName;
+    }
+
+    /**
+     * Get the value of password
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+    public function getMemberAsArray()
+    {
+        return [
+            'id' => $this->id,
+            'username' => $this->userName,
+            'password' => $this->password
+        ];
+    }
 }
