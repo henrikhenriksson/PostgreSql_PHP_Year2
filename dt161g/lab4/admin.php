7965880e3cc4f1e3255a1da1176d21daa1933327
Henrik Henriksson <hehe0601@student.miun.se>
2020-02-24 09:30:41 +0100

    Fixed bug

--- dt161g/lab4/admin.php
+++ dt161g/lab4/admin.php
@@ -11,7 +11,7 @@
  ******************************************************************************/
 session_start();
 // if no session is active (which it only will be if the user is successfully logged in), redirect the user to the index page.
-if (!isset($_SESSION('isAdmin'))) {
+if (!isset($_SESSION['isAdmin'])) {
     header("Location: index.php"); /* Redirect browser */
     exit;
 } else {
